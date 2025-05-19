790d2a3b02bfcaa743da49b13618fa2e2ec5df85
ankit-chy <ankitkchy101226@gmail.com>
2025-05-19 20:16:08 +0530

    Completing_vid_09

---
+++ Complete_Py/Tuts/06_Operators/ops.py
@@ -0,0 +1,80 @@
+'''
+a = 10
+b = 2
+
+# Arithmetic Operators
+print("a+b = ", a+b)
+print("a-b = ", a-b)
+print("a*b = ", a*b)
+print("a/b = ", a/b)
+print("a//b = ", a//b)  # Floor division
+print("a%b = ", a%b)  # Modulus -> remainder
+print("a**b = ", a**b)  # Exponentiation -> power
+
+#Comparison/Conditional Operators
+print("a!=b = ", a!=b)  # Not equal to
+print("a>b = ", a>b)  # Greater than
+print("a<b = ", a<b)  # Less than
+print("a==b = ", a==b)  # Equal to 
+print("a>=b = ", a>=b)  # Greater than or equal to
+print("a<=b = ", a<=b)  # Less than or equal to
+'''
+#Logical Operators
+c = True
+d = False
+# print(c and d)  # Logical AND
+# print(c or d)  # Logical OR
+# print(not c)  # Logical NOT
+
+print("For AND opeator")
+
+print(True and True)  # Logical AND
+print(True and False)  # Logical AND
+print(False and True)  # Logical AND
+print(False and False)  # Logical AND
+
+print("*****************************")
+
+print("For OR opeator")
+
+print(True or True)  # Logical or
+print(True or False)  # Logical or
+print(False or True)  # Logical or
+print(False or False)  # Logical or
+
+print("*****************************")
+
+print("For NOT opeator")
+
+print(not True)  # Logical NOT
+print(not False)  # Logical NOT
+
+
+# Assignment Operators
+
+a = 100
+
+print(a)
+
+# a += 10  # a = a + 10
+# print(a)
+
+
+# Membership Operators
+print("*****************************")
+
+print("For membership opeator")
+
+a = [1, 2, 3, 4, 5]
+print(1 in a)  # Membership operator
+print(1 not in a)  # Membership operator
+print("ankit" in "ankit")  # Membership operator
+
+
+
+# #Identity Operators
+# a = 10
+# b = 10
+# print(a is b)  # Identity operator
+# print(a is not b)  # Identity operator
+
