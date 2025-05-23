83b7cae061a5903871e1e17e47af547607e4f336
ankit-chy <ankitkchy101226@gmail.com>
2025-05-23 07:50:35 +0530

    Completing_Vid_15

---
+++ Complete_Py/Tuts/09_Strings/string.py
@@ -0,0 +1,32 @@
+name = "Ankit1"
+print(name)  # print the value of name
+
+name2 = 'Ankit2'
+print(name2)  # print the value of name2
+
+name3 = '''Ankit3
+can be a multi line string
+and can be used to write long text without using "\\n" for new line'''
+print(name3)  # print the value of name3
+
+# String Indexing
+# String indexing is used to access individual characters in a string
+
+print(name[0])  # print the first character of name - A
+print(name[1])  # print the second character of name - n
+print(name[2])  # print the third character of name- k
+print(name[3])  # print the fourth character of name - i
+print(name[4])  # print the fifth character of name - t
+print(name[5])  # print the sixth character of name - 1
+
+print("*****************************")
+
+# Negative Indexing
+# Negative indexing is used to access individual characters in a string from the end of the string
+print(name[-1])  # print the last character of name - 1
+print(name[-2])  # print the second last character of name - t
+print(name[-3])  # print the third last character of name - k
+print(name[-4])  # print the fourth last character of name - i
+print(name[-5])  # print the fifth last character of name - n
+print(name[-6])  # print the sixth last character of name - A 
+
