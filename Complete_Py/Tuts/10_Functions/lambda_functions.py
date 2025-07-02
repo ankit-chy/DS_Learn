8bc20ee8365e44100ef6a7df1efa35b40454cce2
ankit-chy <ankitkchy101226@gmail.com>
2025-07-02 21:18:39 +0530

    Day05/n_Vid_21_lambda_fn

---
+++ Complete_Py/Tuts/10_Functions/lambda_functions.py
@@ -0,0 +1,5 @@
+square = lambda x : x * x  # lambda function to calculate square of a number
+print(square(5))  # Output: 25
+
+sum = lambda a, b : a + b  # lambda function to calculate sum of two numbers
+print(sum(1, 5))  # Output: 6
