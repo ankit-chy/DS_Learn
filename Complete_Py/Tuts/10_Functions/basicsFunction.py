c3a7d3b7d228366b8139e3de790c3d07cc2164e5
ankit-chy <ankitkchy101226@gmail.com>
2025-07-02 21:08:25 +0530

    Day05/n_Vid_19_Complete

---
+++ Complete_Py/Tuts/10_Functions/basicsFunction.py
@@ -0,0 +1,17 @@
+# a = 10
+# b = 20
+# c = 30
+
+# average=(a+b+c)/3
+
+# print(average)
+
+
+# Function to calculate the average of three numbers
+def calculate_average(num1, num2, num3):
+    return (num1 + num2 + num3) / 3
+
+
+
+print(calculate_average(10, 20, 30))  # Example usage of the function
+print(calculate_average(5, 15, 25))  # Another example usage of the function
