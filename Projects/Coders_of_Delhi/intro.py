7a2c43e6fba42e52b1a098851ec09b817fefd29a
ankit-chy <ankitkchy101226@gmail.com>
2026-07-03 14:18:45 +0530

    added data json file

--- Projects/Coders_of_Delhi/intro.py
+++ Projects/Coders_of_Delhi/intro.py
@@ -7,4 +7,7 @@ def load_data_from_json_file(filename):
         data = json.load(file)
     return data
 
+data = load_data_from_json_file('DS_Learn/Projects/Coders_of_Delhi/data.json')
 
+print(data)
+print("Data loaded successfully from data.json")
