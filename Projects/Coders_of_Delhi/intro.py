c58e6c46a466eefd59d4ef7e8e975a37a2b4aec2
ankit-chy <ankitkchy101226@gmail.com>
2026-07-03 14:34:00 +0530

    updated function to display data in readbale format

--- Projects/Coders_of_Delhi/intro.py
+++ Projects/Coders_of_Delhi/intro.py
@@ -9,5 +9,20 @@ def load_data_from_json_file(filename):
 
 data = load_data_from_json_file('DS_Learn/Projects/Coders_of_Delhi/data.json')
 
-print(data)
-print("Data loaded successfully from data.json")
+#print(data)
+#print("Data loaded successfully from data.json")
+#print(type(data))
+
+
+#function to display the data in a readable format
+def display_data(data):
+    print("Users, their friends and liked pages:\n")
+    for users in data['users']:
+        print(f"ID:{users['id']} {users['name']} is friends with : {users['friends']} and liked pages are : {users['liked_pages']}")
+
+    print("\nPages Information:\n")
+    for page in data['pages']:
+        print(f"{page['id']}: {page['name']}")
+
+print("Displaying data in a readable format:\n")
+display_data(data)
